23ba4f53b2328771cbb6b7ab2e4e4d924c0e034a
Sim <0doogadooga@gmail.com>
2023-07-27 05:14:08 +0900

    feat #11 : pathName에 따른 등록 이동 완료

--- src/components/header/HeaderMenuUlandLi.jsx
+++ src/components/header/HeaderMenuUlandLi.jsx
@@ -3,26 +3,15 @@ import { StHeaderMenuUl, StHeaderMenuLi } from "./stHeaderMenuUlandLi";
 import HeaderMenuLink from "./HeaderMenuLink";
 import HeaderMyDrop from "./HeaderMyDrop";
 import LoginModal from "./LoginModal";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SUPER_URL = "https://supercoding.net/Course/CourseIntro";
 
 const HeaderMenuUlandLi = () => {
   const [openDrop, setOpenDrop] = useState(false);
-  // console.log("openDrop :", openDrop);
 
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  // console.log("token :", token);
-
-  useEffect(() => {
-    // console.log("useEffect");
-    setToken(localStorage.getItem("token"));
-  }, [token]);
-
-  const handleLogout = () => {
-    // console.log("로그아웃 눌러");
-    localStorage.removeItem("token");
-    setOpenDrop(false); // 로그아웃 후 드롭다운을 닫습니다.
-  };
+  const location = useLocation();
+  const navigate = useNavigate();
 
   return (
     <StHeaderMenuUl>
@@ -36,7 +25,12 @@ const HeaderMenuUlandLi = () => {
         <HeaderMenuLink to="/recruit" message="모집 공고" />
       </StHeaderMenuLi>
       <StHeaderMenuLi>
-        <HeaderMenuLink to="/project/upload" message="등록" />
+        {/* <HeaderMenuLink to="/project/upload" message="등록" /> */}
+        {location.pathname === "/" ? (
+          <HeaderMenuLink to="/project/upload" message="등록" />
+        ) : (
+          <HeaderMenuLink to="/recruit/create" message="등록" />
+        )}
       </StHeaderMenuLi>
       <StHeaderMenuLi
         onMouseOver={() => setOpenDrop(true)}
@@ -44,14 +38,6 @@ const HeaderMenuUlandLi = () => {
       >
         <HeaderMenuLink message="MY" />
         {openDrop && <HeaderMyDrop />}
-        {/* {token ? (
-          <HeaderMenuLink message="MY" />
-        ) : (
-          <LoginModal setToken={setToken} />
-        )}
-        {openDrop === false && token === "abc" ? (
-          <HeaderMyDrop handleLogout={handleLogout} />
-        ) : null} */}
       </StHeaderMenuLi>
     </StHeaderMenuUl>
   );
